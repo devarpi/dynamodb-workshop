6514c62a7633b5a0b43c5e171ad41e5d86b582e9
Devarpi Sheth <devarpisheth@macbookair.mynetworksettings.com>
2025-07-14 19:49:40 -0400

    first commit

---
+++ dynamodb-301-workshop/src/dao/dynamodb_adapter.go
@@ -0,0 +1,66 @@
+package dao
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+type DynamoDBAdapter struct {
+	client *dynamodb.Client
+	table  string
+}
+
+func NewDynamoDBAdapter(tableName string) (*DynamoDBAdapter, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return nil, fmt.Errorf("failed to load AWS config: %w", err)
+	}
+
+	client := dynamodb.NewFromConfig(cfg)
+	return &DynamoDBAdapter{
+		client: client,
+		table:  tableName,
+	}, nil
+}
+
+func (a *DynamoDBAdapter) CreateItem(ctx context.Context, item map[string]types.AttributeValue) error {
+	_, err := a.client.PutItem(ctx, &dynamodb.PutItemInput{
+		TableName: aws.String(a.table),
+		Item:      item,
+	})
+	return err
+}
+
+func (a *DynamoDBAdapter) GetItem(ctx context.Context, key map[string]types.AttributeValue) (map[string]types.AttributeValue, error) {
+	result, err := a.client.GetItem(ctx, &dynamodb.GetItemInput{
+		TableName: aws.String(a.table),
+		Key:       key,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return result.Item, nil
+}
+
+func (a *DynamoDBAdapter) UpdateItem(ctx context.Context, key map[string]types.AttributeValue, updateExpression string, expressionValues map[string]types.AttributeValue) error {
+	_, err := a.client.UpdateItem(ctx, &dynamodb.UpdateItemInput{
+		TableName:                 aws.String(a.table),
+		Key:                       key,
+		UpdateExpression:          aws.String(updateExpression),
+		ExpressionAttributeValues: expressionValues,
+	})
+	return err
+}
+
+func (a *DynamoDBAdapter) DeleteItem(ctx context.Context, key map[string]types.AttributeValue) error {
+	_, err := a.client.DeleteItem(ctx, &dynamodb.DeleteItemInput{
+		TableName: aws.String(a.table),
+		Key:       key,
+	})
+	return err
+}
---
+++ dynamodb-301-workshop/test/dynamodb-301-workshop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Dynamodb301Workshop from '../lib/dynamodb-301-workshop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dynamodb-301-workshop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Dynamodb301Workshop.Dynamodb301WorkshopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
